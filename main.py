cb3a0ef4e80624d300ee74f6966590f4a7be48f0
Felipe Ruhland <felipe.ruhland@gmail.com>
2014-05-02 11:54:48 -0300

    Included ping url

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
-import bottle
-from bottle import run, get, template
+from bottle import run, get, template, HTTPResponse, default_app
+
 
 @get('/')
 def home():
     return template('templates/home.html')
 
+
+@get('/ping')
+def ping():
+    return HTTPResponse(status=200)
+
 if __name__ == "__main__":
     run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
-app = bottle.default_app()
+app = default_app()
